87a927da16a494e5ded0ae81830639aaa200d292
Elmeskini Youness <yns.elmeskini@gmail.com>
2025-06-03 17:01:58 +0100

    fix: update CORS configuration to allow requests from the production domain

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://scribo.enopps.com/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie"],
