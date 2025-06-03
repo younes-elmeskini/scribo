d087b0533df1ae42766fac2e0be441bec000d61c
Elmeskini Youness <yns.elmeskini@gmail.com>
2025-06-03 17:00:50 +0100

    refactor: update authenticate middleware to handle token verification and client retrieval

--- src/modules/client/middleware/auth.ts
+++ src/modules/client/middleware/auth.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { Client } from "@prisma/client";
+import prisma from "../../../utils/client";
 
 export interface ClientJwtPayload {
   id: String;
@@ -19,11 +20,11 @@ export const generateToken = (client: Client) => {
   return token;
 };
 
-export const authenticate = (
+export const authenticate = async (
   req: Request,
   res: Response,
   next: NextFunction
-): void  => {
+): Promise<void> => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
 
   if (!token) {
@@ -36,8 +37,21 @@ export const authenticate = (
       throw new Error("JWT_SECRET must be defined");
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET) as ClientJwtPayload;
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    ) as ClientJwtPayload;
     req.client = decoded;
+    
+    const client = await prisma.client.findFirst({
+      where: { id: req.client.id.toString() },
+    })
+    console.log(client);
+    
+    if (!client?.id) {
+      res.status(401).json({ message: "Access denied. Invalid token." });
+      return;
+    }
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid token." });
